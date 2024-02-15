6f4e2921ad67a4d6ca1cff6760ff6b6ff363cdd1
MaheedharPayyavula <113121337+MaheedharPayyavula@users.noreply.github.com>
2024-02-15 18:23:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,49 @@
-noseX=0;
-noseY=0;
-difference = 0;
-rightWristX = 0;
+song = "";
 leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+function preload() {
+    song = loadSound("music.mp3");
+}
 
 function setup() {
-    video = createCapture(VIDEO);
-    video.size(550, 500);
+    canvas = createCanvas(600, 500);
+    canvas.center();
 
-    canvas = createCanvas(550, 550);
-    canvas.position(560,150);
+    video = createCapture(VIDEO);
+    video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
 
-function draw() {
-    background('#969A97');
-
-    document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
-    fill('#F90093');
-    stroke('#F90093');
-    square(noseX, noseY, difference);
+function modelLoaded() {
+    console.log('PoseNet Is Initialized')
 }
 
-function modelLoaded() {
-    console.log('PoseNet Is Initialized!');
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play() {
+   song.play();
+   song.setVolume(1);
+   song.rate(1);  
 }
 
 function gotPoses(results) {
-    if(results.lenght > 0)
-    {
+    if (results.length > 0) {
         console.log(results);
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
-        console.log("noseX = " + noseX +" noseY = " + noseY);
-
         leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY)
+        
         rightWristX = results[0].pose.rightWrist.x;
-        difference = floor(leftWristX - rightWristX);
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY)
 
-        console.log("leftWristX = " + " rightWristX = "+ rightWristX + " difference = " + difference);
     }
 }
