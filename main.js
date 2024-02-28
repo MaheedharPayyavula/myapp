cc82f48d11606e6299dbf8ca90ebfb28629722bc
MaheedharPayyavula <113121337+MaheedharPayyavula@users.noreply.github.com>
2024-02-28 18:31:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,19 @@
-song = "";
-leftWristX = 0;
-leftWristY = 0;
-
-rightWristX = 0;
-rightWristY = 0;
+img = "";
 
 function preload() {
-    song = loadSound("music.mp3");
+    img = loadImage('dog_cat.jpg');
 }
 
 function setup() {
-    canvas = createCanvas(600, 500);
+    canvas = createCanvas(640, 420);
     canvas.center();
-
-    video = createCapture(VIDEO);
-    video.hide();
-
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-}
-
-function modelLoaded() {
-    console.log('PoseNet Is Initialized')
 }
 
 function draw() {
-    image(video, 0, 0, 600, 500);
-}
-
-function play() {
-   song.play();
-   song.setVolume(1);
-   song.rate(1);  
-}
-
-function gotPoses(results) {
-    if (results.length > 0) {
-        console.log(results);
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY)
-        
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY)
-
-    }
+    image(img, 0, 0, 640, 420);
+    fill('#FF0000');
+    text("Dog", 45, 75);
+    noFill();
+    stroke('#FF0000');
+    rect(30, 60, 450, 350 );
 }
